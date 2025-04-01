fc3cba1baf780216189c0849257237f8ea4ff106
Marc Jakobi <marc.jakobi@tiko.energy>
2025-04-01 15:33:59 +0200

    fix(windows): disable gpg signing for windows MSVC

--- lux-cli/src/upload.rs
+++ lux-cli/src/upload.rs
@@ -8,6 +8,8 @@ use lux_lib::{
 
 #[derive(Args)]
 pub struct Upload {
+    /// The protocol to use when signing upload artefacts
+    #[cfg(not(target_env = "msvc"))]
     #[arg(long, default_value_t)]
     sign_protocol: SignatureProtocol,
 }
@@ -15,8 +17,14 @@ pub struct Upload {
 pub async fn upload(data: Upload, config: Config) -> Result<()> {
     let project = Project::current()?.unwrap();
 
+    #[cfg(not(target_env = "msvc"))]
     ProjectUpload::new(project, &config)
         .sign_protocol(data.sign_protocol)
+        .upload_to_luarocks()
+        .await?;
+
+    #[cfg(target_env = "msvc")]
+    ProjectUpload::new(project, &config)
         .upload_to_luarocks()
         .await?;
 
