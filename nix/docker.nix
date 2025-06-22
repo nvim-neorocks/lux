23195047e8596a0616a9df7cc73dfd2079a69c78
Henry <cootshk@duck.com>
2025-06-21 22:20:50 -0500

    move to buildEnv

--- nix/docker.nix
+++ nix/docker.nix
@@ -1,23 +1,33 @@
 self: final: prev: let
   pkgs = final;
   lux-cli-docker = with pkgs;
-    pkgs.dockerTools.buildImage {
+    dockerTools.buildImage {
       name = "lux";
       tag = "cli";
+      copyToRoot = buildEnv {
+        name = "lux-cli-root";
+        paths = [lux-cli];
+        pathsToLink = ["/bin"];
+      };
       config = {
-        Cmd = ["${lux-cli}/bin/lux"];
+        Cmd = ["/bin/lux"];
       };
     };
   mk-lux-lua-docker = lua_pkg:
-    pkgs.dockerTools.buildImage {
-      name = "lux";
-      fromImage = lux-cli-docker;
-      tag = "${lua_pkg.pname}";
-      contents = [pkgs.lux-cli lua_pkg];
-      config = {
-        Cmd = ["${pkgs.lux-cli}/bin/lux" "--lua" "${lua_pkg.pname}"];
+    with pkgs;
+      dockerTools.buildImage {
+        name = "lux";
+        fromImage = lux-cli-docker;
+        tag = "${lua_pkg.pname}";
+        copyToRoot = buildEnv {
+          name = "${lua_pkg.pname}-root";
+          paths = [lux-cli lua_pkg];
+          pathsToLink = ["/bin" "/lib"];
+        };
+        config = {
+          Cmd = ["/bin/lux"];
+        };
       };
-    };
 in {
   lux-cli-docker = lux-cli-docker;
   lux-lua51-docker = mk-lux-lua-docker pkgs.lux-lua51;
