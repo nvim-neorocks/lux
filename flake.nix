19cf02733f21c19e1eb347ae98e12cbddc2f5ecc
Henry <cootshk@duck.com>
2025-06-21 23:03:00 -0500

    fix the flake

--- flake.nix
+++ flake.nix
@@ -112,8 +112,10 @@
           default = with inputs; import ./nix/overlay.nix {inherit self crane;};
           docker = with inputs;
             import ./nix/docker.nix {
-              inherit self;
-              date = builtins.substring 0 8 self.lastModifiedDate;
+              inherit
+                self
+                ;
+              # date = builtins.substring 0 8 self.lastModifiedDate;
             };
         };
       };
