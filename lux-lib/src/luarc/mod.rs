d0d048d9d1d2dfa031a84c092592ba556e556676
Ricardo Casía <31012661+rcasia@users.noreply.github.com>
2025-07-11 04:45:55 +0200

    feat: use project struct

--- lux-lib/src/luarc/mod.rs
+++ lux-lib/src/luarc/mod.rs
@@ -1,11 +1,17 @@
-pub fn update_luarc() -> Result<(), ()> {
-    let file = generate_luarc();
+use crate::project::Project;
+use crate::project::ProjectRoot;
 
-    std::fs::write(".luarc.json", file).expect("failed to write .luarc.json file");
+pub fn update_luarc() -> Result<(), ()> {
+    let project = Project::current_or_err().expect("failed to get current project");
+    let luarc_path = project.luarc_path();
+    let file = generate_luarc(project.root());
+
+    std::fs::write(&luarc_path, file)
+        .expect(format!("failed to write {} file", luarc_path.display()).as_str());
     Ok(())
 }
 
-fn generate_luarc() -> String {
+fn generate_luarc(project_root: &ProjectRoot) -> String {
     let mut content = String::new();
     content.push_str("{");
     content.push_str("\n    \"workspace.library\": []\n");
@@ -16,9 +22,11 @@ fn generate_luarc() -> String {
 #[cfg(test)]
 mod test {
 
+    use crate::project::ProjectRoot;
+
     #[test]
     fn test_generate_luarc() {
-        let content = super::generate_luarc();
+        let content = super::generate_luarc(&ProjectRoot::new());
         assert_eq!(
             content,
             r#"{
