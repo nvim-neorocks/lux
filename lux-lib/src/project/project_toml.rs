71129ef9b81b3c7f46806e406abaf94160280304
Marc Jakobi <marc.jakobi@tiko.energy>
2025-08-25 18:23:47 +0200

    feat: add build backend deps when generating from local project toml

--- lux-lib/src/project/project_toml.rs
+++ lux-lib/src/project/project_toml.rs
@@ -714,11 +714,26 @@ version = "{}""#,
             template.push(Dependencies(&dependencies).display_lua());
         }
 
-        match self.internal.build_dependencies {
-            Some(ref build_dependencies) if !build_dependencies.is_empty() => {
-                template.push(BuildDependencies(build_dependencies).display_lua());
-            }
-            _ => {}
+        let mut build_dependencies = self
+            .internal
+            .build_dependencies
+            .as_ref()
+            .cloned()
+            .unwrap_or_default();
+
+        let build_backend_dependency = self
+            .internal
+            .build
+            .build_type
+            .as_ref()
+            .and_then(|build_type| build_type.luarocks_build_backend());
+
+        if let Some(build_backend_dependency) = build_backend_dependency {
+            build_dependencies.push(build_backend_dependency);
+        }
+
+        if !build_dependencies.is_empty() {
+            template.push(BuildDependencies(&build_dependencies).display_lua());
         }
 
         match self.internal.external_dependencies {
